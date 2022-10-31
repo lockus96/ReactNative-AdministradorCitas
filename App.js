a2b1093addc55f677f4c92a5e28968f5d5116e37
lockus96 <angelmcarrizo@gmail.com>
2022-10-30 22:37:18 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View, } from 'react-native';
+
+
+const App = () => {
+
+
+  return (
+    <Text>Hola Mundo</Text>
+  );
+};
+
+
+export default App;
